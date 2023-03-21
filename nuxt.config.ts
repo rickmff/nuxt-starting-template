77aaf56fce1b1223acf87a844497ddff32bde9bf
rickmff <rickmff@gmail.com>
2023-03-21 00:29:53 -0300

    initial setup

---
+++ nuxt.config.ts
@@ -0,0 +1,41 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+import { resolve } from 'path'
+import eslintPlugin from 'vite-plugin-eslint'
+
+export default defineNuxtConfig({
+  alias: {
+    '@': resolve(__dirname, '/')
+  },
+  css: [
+    '~/assets/main.scss'
+  ],
+  vite: {
+    plugins: [
+      eslintPlugin()
+    ]
+  },
+  modules: [
+    '@nuxtjs/tailwindcss',
+    '@nuxtjs/sentry'
+  ],
+  buildModules: [
+    '@nuxtjs/pwa'
+  ],
+  /*   pwa: {
+    meta: {
+      name: 'Nuxt Vite',
+      description: 'Nuxt Vite'
+    },
+    manifest: {
+      name: 'My Awesome App',
+      lang: 'fa',
+      useWebmanifestExtension: false
+    }
+  }, */
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {}
+    }
+  }
+})
---
+++ pages/welcomeConsole.vue
@@ -0,0 +1,7 @@
+<template>
+  <h1>Pudim</h1>
+</template>
+
+<script setup lang="ts">
+
+</script>
